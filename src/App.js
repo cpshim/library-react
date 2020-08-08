7bd4286cb7f68704d568e41a380a5af9580c8e27
Chris <chris.shim1999@hotmail.com>
2020-08-08 18:13:02 -0400

    React automatically re-renders new books in the myLibrary array to display book info in a grid layout.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.numOfPages = pages;
+  this.read = read;
+}
+
+class BookTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myLibrary: [
+        { title: "Test Title", author: "Bob", numOfPages: 4 },
+        { title: "Test Title 2", author: "Bob", numOfPages: 4 },
+        { title: "Test Title 3", author: "Bob", numOfPages: 4 }
+      ],
+    };
+  }
+
+  addNewBook() {
+    let newBook = new Book("Foo", "Mr. Bar", 25, false);
+    let currentLibrary = this.state.myLibrary;
+    //Do it this way since push function returns an integer so can't set
+    //state.myLibrary: currentLibrary.push() or else map function will fail
+    currentLibrary.push(newBook);
+    this.setState({
+      myLibrary: currentLibrary,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="book-area">
+          {this.state.myLibrary.map(book =>
+            <div className="card">
+              {book.title} by {book.author}
+              <hr></hr>
+              Number of Pages: {book.numOfPages}
+            </div>)
+          }
+        </div>
+        <button className="new-book-btn" onClick={() => this.addNewBook()}>Click to add</button>
+      </div>
+    )
+  }
+}
+
+class NewBookBtn extends React.Component {
+  state = {
+    show: false
+  }
+
+  showForm(){
+    this.setState({
+      show: true
+    });
+  }
+
+  hideForm(){
+    this.setState({
+      show: false
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="new-book-btn" onClick={() => this.showForm()}>New Book</button>
+      </div>
+    )    
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>My Handy Library</h1>
+        </header>
+        <body>
+          <BookTable />
+          <NewBookBtn />
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
