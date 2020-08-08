55ccebc1816e895995f2048322658cb096e29357
Chris <chris.shim1999@hotmail.com>
2020-08-08 18:49:53 -0400

    Added a form so users can input a title, author, and number of pages and click submit to log a new book with info submitted.

--- src/App.js
+++ src/App.js
@@ -14,15 +14,23 @@ class BookTable extends React.Component {
     super(props);
     this.state = {
       myLibrary: [
-        { title: "Test Title", author: "Bob", numOfPages: 4 },
-        { title: "Test Title 2", author: "Bob", numOfPages: 4 },
-        { title: "Test Title 3", author: "Bob", numOfPages: 4 }
+        { title: "Test Title", author: "Bob", numOfPages: 4, read: true },
+        { title: "Test Title 2", author: "Bob", numOfPages: 4, read: false },
+        { title: "Test Title 3", author: "Bob", numOfPages: 4, read: true }
       ],
+      title: '',
+      author: '',
+      numOfPages: '',
+      read: false,
     };
   }
 
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+ };
+
   addNewBook() {
-    let newBook = new Book("Foo", "Mr. Bar", 25, false);
+    let newBook = new Book(this.state.title, this.state.author, this.state.numOfPages, this.state.read);
     let currentLibrary = this.state.myLibrary;
     //Do it this way since push function returns an integer so can't set
     //state.myLibrary: currentLibrary.push() or else map function will fail
@@ -40,39 +48,39 @@ class BookTable extends React.Component {
             <div className="card">
               {book.title} by {book.author}
               <hr></hr>
-              Number of Pages: {book.numOfPages}
+              <p>Number of Pages: {book.numOfPages}</p>
+              Finished Reading: {book.read ? "Yes":"No"}
             </div>)
           }
         </div>
+        <form className="new-book-form">
+          <label htmlFor="Title">Title</label>
+          <input
+            type="text"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+ 
+          <label htmlFor="Author">Author</label>
+          <input
+            type="text"
+            name="author"
+            value={this.state.author}
+            onChange={this.handleChange}
+          />
+
+          <label htmlFor="Pages">Pages</label>
+          <input
+            type="text"
+            name="numOfPages"
+            value={this.state.numOfPages}
+            onChange={this.handleChange}
+          />
+        </form>
         <button className="new-book-btn" onClick={() => this.addNewBook()}>Click to add</button>
       </div>
     )
-  }
-}
-
-class NewBookBtn extends React.Component {
-  state = {
-    show: false
-  }
-
-  showForm(){
-    this.setState({
-      show: true
-    });
-  }
-
-  hideForm(){
-    this.setState({
-      show: false
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <button className="new-book-btn" onClick={() => this.showForm()}>New Book</button>
-      </div>
-    )    
   }
 }
 
@@ -85,7 +93,6 @@ class App extends React.Component {
         </header>
         <body>
           <BookTable />
-          <NewBookBtn />
         </body>
       </div>
     );
